82f560d6dea42bbe772e5335608923a3306d560a
masroof-maindak <mujtaba.asim.amin@gmail.com>
2024-08-10 20:42:29 +0500

    patch: less wasted space (rofi/waybar/foot) + stop maintaining signal hack

--- .config/rofi/scripts/launcher.sh
+++ .config/rofi/scripts/launcher.sh
@@ -1,9 +1,3 @@
 #!/usr/bin/env bash
 
-FILE=launcher.rasi
-
-if [ "$1" = "-w" ]; then
-  FILE=launcher-wayland.rasi
-fi
-
-rofi -show drun -theme ~/.config/rofi/${FILE}
+rofi -show drun -theme ~/.config/rofi/launcher.rasi
