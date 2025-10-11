342f6dc74f116d682681074d24d0ec7a95ed8068
masroof-maindak <mujtaba.asim.amin@gmail.com>
2025-10-11 23:03:28 +0500

    fix(fish): sudo-prefix bind
    Exists by default; defining it instead disabled it.

--- .config/fish/conf.d/keybindings.fish
+++ .config/fish/conf.d/keybindings.fish
@@ -1,6 +1,3 @@
-set -g fish_completion_case_sensitive no
-
-bind alt-s 'commandline -P "sudo "'
 bind alt-n nvim
 bind alt-z zi
 bind alt-l 'set old_tty (stty -g); stty sane; lfcd; stty $old_tty; commandline -f repaint'
