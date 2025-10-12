3a79112b067fbc0c715b3f901d34095fc74ad50b
masroof-maindak <mujtaba.asim.amin@gmail.com>
2025-10-12 19:09:20 +0500

    feat(fish): use fish_git_prompt) for git info

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -2,6 +2,15 @@ set -g fish_prompt_pwd_dir_length 0
 set -g fish_transient_prompt 1
 
 function fish_prompt
+    # Reference:
+    #   https://fishshell.com/docs/current/prompt.html
+    #   https://fishshell.com/docs/current/cmds/fish_git_prompt.html
+
+    set -g __fish_git_prompt_show_informative_status false
+    set -g __fish_git_prompt_showupstream none
+    set -g __fish_git_prompt_showdirtystate 1
+    set -g __fish_git_prompt_showuntrackedfiles 1
+
     set -l prompt_content
 
     if contains -- --final-rendering $argv
@@ -10,7 +19,7 @@ function fish_prompt
     else
         # Final
         set prompt_content (set_color blue)(prompt_pwd)
-        set -l branch (fish_git_branch)
+        set -l branch (fish_git_prompt)
         set prompt_content "$prompt_content"(set_color cyan --bold)"$branch"
     end
 
@@ -28,16 +37,4 @@ function fish_right_prompt
     end
 
     set_color normal
-end
-
-function fish_git_branch
-    set -l branch (git branch --show-current 2>/dev/null)
-    if test -n "$branch"
-        echo -n " "
-        set -l git_status_output (git diff --shortstat 2>/dev/null | tail -n1)
-        if test -n "$git_status_output"
-            echo -n (set_color --reverse)"*"
-        end
-        echo -n "("$branch")"
-    end
 end
