f41b015bdb06d5438a8d6bbdb883ab0b61b7923d
masroof-maindak <mujtaba.asim.amin@gmail.com>
2025-10-15 10:47:38 +0500

    refactor(fish): move declarations out
    Former-commit-id: 7d4b7bc9ad63d3ce175baa8f74efcdba43124ef3
    Former-commit-id: 2f025626369ae18eab128015de01ce34ff995f3c

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -1,16 +1,15 @@
 set -g fish_prompt_pwd_dir_length 0
 set -g fish_transient_prompt 1
 
+# Reference:
+#   https://fishshell.com/docs/current/prompt.html
+#   https://fishshell.com/docs/current/cmds/fish_git_prompt.html
+set -g __fish_git_prompt_show_informative_status false
+set -g __fish_git_prompt_showupstream none
+set -g __fish_git_prompt_showdirtystate 1
+set -g __fish_git_prompt_showuntrackedfiles 1
+
 function fish_prompt
-    # Reference:
-    #   https://fishshell.com/docs/current/prompt.html
-    #   https://fishshell.com/docs/current/cmds/fish_git_prompt.html
-
-    set -g __fish_git_prompt_show_informative_status false
-    set -g __fish_git_prompt_showupstream none
-    set -g __fish_git_prompt_showdirtystate 1
-    set -g __fish_git_prompt_showuntrackedfiles 1
-
     set -l prompt_content
 
     if contains -- --final-rendering $argv
@@ -23,8 +22,7 @@ function fish_prompt
         set prompt_content "$prompt_content"(set_color cyan --bold)"$branch"
     end
 
-    set -l out "$prompt_content"(set_color normal)" \$ "
-    echo -n "$out"
+    echo -n "$prompt_content"(set_color normal)" \$ "
 end
 
 function fish_right_prompt
