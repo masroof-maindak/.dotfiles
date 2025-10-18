155ce26baf608eb4878dd58e209166a9dfde71ce
masroof-maindak <mujtaba.asim.amin@gmail.com>
2025-10-19 02:14:36 +0500

    fix(fish): alt+z to open zoxide
    Ref: https://github.com/ajeetdsouza/zoxide/discussions/791
    
    Former-commit-id: d978d9df0bf21ffb759995c6b02b2f6fb89f298d
    Former-commit-id: 6480d8ca25b7c2835376324c18bd2f4d3a56b33c

--- .config/fish/conf.d/keybindings.fish
+++ .config/fish/conf.d/keybindings.fish
@@ -1,4 +1,4 @@
 bind alt-n nvim
-bind alt-z zi
+bind alt-z 'zi && commandline --function repaint'
 bind alt-l 'set old_tty (stty -g); stty sane; lfcd; stty $old_tty; commandline -f repaint'
 bind alt-m make
