b22b37244539f6be0d2881adc1390d828366017e
masroof-maindak <mujtaba.asim.amin@gmail.com>
2025-10-19 02:15:17 +0500

    feat(fish): open tmux w/ alt+t
    Former-commit-id: 1017cbb1e1af56ab841dbcb9963d4d1026f58b54
    Former-commit-id: da1a3d7f317de96a4a8677fd23781081ff1ac482

--- .config/fish/conf.d/keybindings.fish
+++ .config/fish/conf.d/keybindings.fish
@@ -2,3 +2,4 @@ bind alt-n nvim
 bind alt-z 'zi && commandline --function repaint'
 bind alt-l 'set old_tty (stty -g); stty sane; lfcd; stty $old_tty; commandline -f repaint'
 bind alt-m make
+bind alt-t tmux
