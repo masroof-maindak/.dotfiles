0c026ac1548529407da3e257ad04c0aa16280bf8
masroof-maindak <mujtaba.asim.amin@gmail.com>
2025-10-23 00:40:02 +0500

    feat: use sccache for rust
    Literal magic.
    
    
    Former-commit-id: e94f2a939c11a809ffa9c146416222e1f72f81dd
    Former-commit-id: de6325d40a462df3c748c54f1d6346b1b1e8c0c0

--- .config/fish/conf.d/environment.fish
+++ .config/fish/conf.d/environment.fish
@@ -18,6 +18,7 @@ set -x XRESOURCES "$XDG_CONFIG_HOME/x11/xresources"
 
 set -x CARGO_HOME "$XDG_DATA_HOME/cargo"
 set -x RUSTUP_HOME "$XDG_DATA_HOME/rustup"
+set -x RUSTC_WRAPPER "sccache"
 
 set -x GOPATH "$XDG_DATA_HOME/go"
 set -x GOBIN "$GOPATH/bin"
