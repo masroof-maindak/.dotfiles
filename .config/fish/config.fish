ec6e476a9cf873600aed3225edc1cdd88846c252
masroof-maindak <mujtaba.asim.amin@gmail.com>
2025-11-01 19:17:29 +0500

    patch(fish): move conf files to dedicated dir
    So plugins can use conf.d as they want and we can itignore the whole thing

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,7 +1,4 @@
 set -g fish_greeting ""
 set -g fish_completion_case_sensitive no
 
-# Source configuration snippets
-for conf_file in ~/.config/fish/conf.d/*.fish
-    source $conf_file
-end
+for f in ~/.config/fish/user-conf/*.fish; source $f; end
