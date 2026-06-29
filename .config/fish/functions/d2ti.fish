cddf5bd1bf8f55d97941101bbd6fcfdd8a4dc942
masroof-maindak <mujtaba.asim.amin@gmail.com>
2026-06-29 15:11:59 +0500

    chore(d2t): add file preview

--- .config/fish/functions/d2ti.fish
+++ .config/fish/functions/d2ti.fish
@@ -1,3 +1,3 @@
 function d2ti --description="Dir 2 Txt Interactive; fzf + wl-copy"
-    d2t -- $(fzf) | wl-copy
+    d2t -- $(fzf --preview 'bat -P -p --color=always {} --line-range=:250') | wl-copy
 end
