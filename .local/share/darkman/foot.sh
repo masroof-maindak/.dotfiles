d06fa7ce4854753372639a93f39f28ddaf567f97
masroof-maindak <mujtaba.asim.amin@gmail.com>
2026-08-18 00:26:27 +0500

    fix(darkman): foot bootup

--- .local/share/darkman/foot.sh
+++ .local/share/darkman/foot.sh
@@ -1,7 +1,18 @@
 #!/bin/sh
 
+config="$HOME/.config/foot/foot.ini"
+
 case "$1" in
-dark) pkill -USR1 foot ;;
-light) pkill -USR2 foot ;;
+dark)
+    NUM=1
+    THEME=dark
+    ;;
+light)
+    NUM=2
+    THEME=light
+    ;;
 default) exit 1 ;;
 esac
+
+sed -i "s|^initial-color-theme=.*|initial-color-theme=$THEME|" "$config"
+killall -SIGUSR$NUM foot
